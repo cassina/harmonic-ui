1cfb41fb5a758b86c69fe61192f7f80284868705
cassina <bernardocassina@gmail.com>
2023-10-10 17:24:28 -0600

    fix: correct text style for label.

--- src/lib/components/notes/Label/Label.style.ts
+++ src/lib/components/notes/Label/Label.style.ts
@@ -2,7 +2,7 @@ import {BaseStyle} from "@interfaces";
 import {convertBaseStyle} from "@utils/index";
 
 const labelBase: BaseStyle = {
-    font: 'text-primary-50 text-xs',
+    font: 'text-primary-950 text-xs',
     margin: 'm-1',
     display: 'block',
 }
