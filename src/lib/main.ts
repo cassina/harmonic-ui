d839990e1dc9c0f957cfe91a9802fc90d20e8ab6
Bernardo Cassina Buerba <bernardocassina@gmail.com>
2023-11-01 11:24:07 -0500

    Resolve: 25 add social svgs (#26)
    * feat: create SocialLoginSVG component, improve Button and main.ts readability
    
    * feat: add story for button with icon, refactor name to SocialSVG

--- src/lib/main.ts
+++ src/lib/main.ts
@@ -1,9 +1,15 @@
+// Notes
 export * from "./components/notes/Button/Button";
 export * from "./components/notes/Checkbox/Checkbox";
 export * from "./components/notes/SVG/SVG";
 export * from "./components/notes/Label/Label";
+
+// Chords
+export * from "./components/notes/SocialSVG/SocialSVG";
 export * from "./components/chords/TextInput/TextInput";
 export * from "./components/chords/ChoiceInput/ChoiceInput";
+
+// Other
 import withHarmony from './config/with-harmony';
 
 export {
