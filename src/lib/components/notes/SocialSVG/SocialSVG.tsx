fd0d0147f2b2af219cb61f25ecfe6a373cf1a7f7
Bernardo Cassina Buerba <bernardocassina@gmail.com>
2023-11-01 11:24:07 -0500

    Resolve: 25 add social svgs (#26)
    * feat: create SocialLoginSVG component, improve Button and main.ts readability
    
    * feat: add story for button with icon, refactor name to SocialSVG

---
+++ src/lib/components/notes/SocialSVG/SocialSVG.tsx
@@ -0,0 +1,66 @@
+import {
+    APPLE_SVG_PATH,
+    FACEBOOK_SVG_PATH,
+    GITHUB_SVG_PATH,
+    GOOGLE_SVG_PATH,
+    TWITTER_SVG_PATH
+} from "@components/notes/SocialSVG/SocialSVG.config";
+
+interface SocialSVGProps {
+    id: string;
+    viewBox: string;
+    path: string;
+    width?: string;
+    height?: string;
+}
+
+export const SocialSVG = (props: SocialSVGProps) => {
+    const {
+        id,
+        viewBox,
+        path,
+        width = '4',
+        height = '4'
+    } = props;
+
+    return(<svg id={id}
+         className={`w-${width} h-${height}`}
+         aria-hidden="true"
+         xmlns="http://www.w3.org/2000/svg"
+         fill="white"
+         viewBox={viewBox}>
+        <path d={path} fillRule="evenodd" clipRule="evenodd" />
+    </svg>)
+};
+
+export const FacebookSVG = () => (
+    <SocialSVG id="facebook-icon"
+               viewBox="0 0 8 19"
+               path={FACEBOOK_SVG_PATH} />
+);
+
+export const TwitterSVG = () => (
+    <SocialSVG id="twitter-icon"
+               viewBox="0 0 20 17"
+               path={TWITTER_SVG_PATH} />
+);
+
+export const GithubSVG = () => (
+    <SocialSVG id="github-icon"
+               viewBox="0 0 20 20"
+               path={GITHUB_SVG_PATH} />
+);
+
+export const AppleSVG = () => (
+    <SocialSVG id="apple-icon"
+               viewBox="0 0 384 512"
+               path={APPLE_SVG_PATH} />
+);
+
+export const GoogleSVG = () => {
+    return(
+        <SocialSVG id="google-icon"
+                   viewBox="0 0 18 19"
+                   path={GOOGLE_SVG_PATH} />
+    );
+};
