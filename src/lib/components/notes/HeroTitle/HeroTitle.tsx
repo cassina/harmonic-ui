6ad24fb6f009a6a54b25e4793d5545862333b266
cassina <bernardocassina@gmail.com>
2023-11-10 22:07:05 -0600

    style: improve Hero styles to follow project conventions

--- src/lib/components/notes/HeroTitle/HeroTitle.tsx
+++ src/lib/components/notes/HeroTitle/HeroTitle.tsx
@@ -1,7 +1,10 @@
 import {ComponentProps} from "react";
-import {getHeroTitleStyle} from "@components/notes/HeroTitle/HeroTitle.style";
+import {mergeClasses} from "@utils/index";
+import {getHeroTitleStyle} from "./HeroTitle.style";
 
-export const HeroTitle = ({ children }: ComponentProps<'h1'>) =>
-    <h1 className={getHeroTitleStyle()}>
-        {children}
-    </h1>;
+export const HeroTitle = ({ children, className }: ComponentProps<'h1'>) => {
+    const theme = getHeroTitleStyle();
+    const styles = mergeClasses(className, theme);
+
+    return(<h1 className={styles}>{children}</h1>);
+};
