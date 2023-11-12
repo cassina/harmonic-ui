e1246c12cbd911d77b23fc00e85be5d080bde124
cassina <bernardocassina@gmail.com>
2023-11-12 09:34:06 -0600

    chore: update main.ts imports

--- src/lib/main.ts
+++ src/lib/main.ts
@@ -1,23 +1,27 @@
 // Notes
-export * from "./components/notes/Button/Button";
-export * from "./components/notes/Checkbox/Checkbox";
-export * from "./components/notes/HeroSubtitle/HeroSubtitle";
-export * from "./components/notes/HeroTitle/HeroTitle";
-export * from "./components/notes/Label/Label";
-export * from "./components/notes/SocialSVG/SocialSVG";
-export * from "./components/notes/SVG/SVG";
+export * from './components/notes/Button/Button';
+export * from './components/notes/Checkbox/Checkbox';
+export * from './components/notes/HeroSubtitle/HeroSubtitle';
+export * from './components/notes/HeroTitle/HeroTitle';
+export * from './components/notes/Label/Label';
+export * from './components/notes/Logo/Logo';
+export * from './components/notes/MobileMenuButton/MobileMenuButton';
+export * from './components/notes/SocialSVG/SocialSVG';
+export * from './components/notes/SVG/SVG';
 
 // Chords
 export * from "./components/chords/ChoiceInput/ChoiceInput";
 export * from "./components/chords/HeroButtons/HeroButtons";
 export * from "./components/chords/TextInput/TextInput";
 
+// Progressions
+export * from './components/progressions/Navbar/Navbar';
+
 // Structures
 export * from "./components/structures/Hero/Hero";
 
-// Other
-import withHarmony from './config/with-harmony';
+// Config
+export * from './components/notes/SVG/SVG.config';
 
-export {
-    withHarmony
-};
+import withHarmony  from './config/with-harmony';
+export { withHarmony };
