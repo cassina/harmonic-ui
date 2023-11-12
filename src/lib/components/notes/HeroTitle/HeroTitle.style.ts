a137eb5e6dd511a6d77a51304dcd5f8b5451f165
cassina <bernardocassina@gmail.com>
2023-11-12 09:35:19 -0600

    style: update HeroTitle styles

--- src/lib/components/notes/HeroTitle/HeroTitle.style.ts
+++ src/lib/components/notes/HeroTitle/HeroTitle.style.ts
@@ -2,7 +2,7 @@ import {BaseStyle} from "@interfaces";
 import {convertBaseStyle} from "@utils/index";
 
 const heroTitleStyle: BaseStyle = {
-    font: 'text-[2rem] 2xl:text-6xl font-bold tracking-wide',
+    font: 'text-[2rem] lg:text-5xl 2xl:text-6xl font-bold tracking-wide',
     margin: 'mb-10'
 };
 
