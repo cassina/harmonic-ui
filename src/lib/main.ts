ba52c04f8cb8ebe88b419032efdee134d36d1dba
cassina <bernardocassina@gmail.com>
2023-11-14 22:12:20 -0600

    chore: normalize main.ts import paths

--- src/lib/main.ts
+++ src/lib/main.ts
@@ -1,18 +1,18 @@
 // Notes
 export * from './components/notes/Button/Button';
 export * from './components/notes/Checkbox/Checkbox';
-export * from '@components/notes/SectionTitle/SectionTitle';
+export * from './components/notes/SectionTitle/SectionTitle';
 export * from './components/notes/HeroSubtitle/HeroSubtitle';
 export * from './components/notes/HeroTitle/HeroTitle';
 export * from './components/notes/Label/Label';
 export * from './components/notes/Logo/Logo';
-export * from '@components/chords/MobileMenuButton/MobileMenuButton';
-export * from '@components/notes/SocialIcon/SocialIcon';
-export * from '@components/notes/CheckboxShape/CheckboxShape';
+export * from './components/chords/MobileMenuButton/MobileMenuButton';
+export * from './components/notes/SocialIcon/SocialIcon';
+export * from './components/notes/CheckboxShape/CheckboxShape';
 
 // Chords
 export * from "./components/chords/ChoiceInput/ChoiceInput";
-export * from "@components/structures/HeroButtons/HeroButtons";
+export * from "./components/structures/HeroButtons/HeroButtons";
 export * from "./components/chords/TextInput/TextInput";
 
 // Structures
