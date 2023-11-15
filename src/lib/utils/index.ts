9a1ceb8c3136fe1c75345308ca6771f67c8bd430
cassina <bernardocassina@gmail.com>
2023-11-15 09:32:01 -0600

    enhancement: update dependencies, remove chroma and unused function

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,9 +1,4 @@
-import chroma from 'chroma-js';
 import {BaseStyle} from "@interfaces";
-export function getContrastColor(bgColor: string) {
-    const luminance = chroma(bgColor).luminance();
-    return luminance > 0.5 ? 'black' : 'white';
-}
 
 // Helper function to convert BaseStyles to string format
 export const convertBaseStyle = (theme: BaseStyle): string => {
