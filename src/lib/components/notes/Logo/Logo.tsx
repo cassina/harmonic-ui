29ab54805325fba670348cb7cd6a94191862d176
cassina <bernardocassina@gmail.com>
2023-11-15 09:02:28 -0600

    test: fix tests and add props to implementation

--- src/lib/components/notes/Logo/Logo.tsx
+++ src/lib/components/notes/Logo/Logo.tsx
@@ -2,10 +2,11 @@ import {LogoProps} from "@interfaces";
 import {mergeClasses} from "@utils/index";
 
 export const Logo = ({ className, src, alt, ...props }: LogoProps) => {
-    const classes = mergeClasses(className, 'h-8 w-auto')
+    const classes = mergeClasses(className, 'h-8 w-auto');
     return (
         <div className={'flex flex-shrink-0 items-center'}>
             <img
+                {...props }
                 className={classes}
                 src={src}
                 alt={alt}
