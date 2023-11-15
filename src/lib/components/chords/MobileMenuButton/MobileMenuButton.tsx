85d60689ef923662e6ae20eb358fbde3fd18d9ec
cassina <bernardocassina@gmail.com>
2023-11-14 22:31:58 -0600

    enhancement: change Menu SVG Icons for React Icons Themify

--- src/lib/components/chords/MobileMenuButton/MobileMenuButton.tsx
+++ src/lib/components/chords/MobileMenuButton/MobileMenuButton.tsx
@@ -1,9 +1,8 @@
 import React from "react";
+import { TfiMenu, TfiClose } from "react-icons/tfi";
 
 import {MobileMenuButtonProps} from "@interfaces";
-import {SVGPaths} from "@config/svg-paths";
 
-import {Icon} from "@components/notes/Icon/Icon";
 
 export const MobileMenuButton = ({ isOpen, toggleMobileMenu }: MobileMenuButtonProps) => {
     return (
@@ -18,25 +17,12 @@ export const MobileMenuButton = ({ isOpen, toggleMobileMenu }: MobileMenuButtonP
                 <span className="absolute -inset-0.5"></span>
                 <span className="sr-only">Open main menu</span>
 
-                {/* Icon when menu is closed */}
-                <Icon
-                    ariaLabel={'Open Mobile Menu'}
-                    className={`block h-6 w-6 ${isOpen ? 'hidden' : 'block'}`}
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    pathData={SVGPaths.hamburger}
-                />
-
-                {/* Icon when menu is closed */}
-                <Icon
-                    ariaLabel={'Close Mobile Menu'}
-                    className={`h-6 w-6 ${!isOpen ? 'hidden' : 'block'}`}
-                    fill="currentColor"
-                    pathData={SVGPaths.cancel}
-                />
+                {
+                    isOpen ?
+                        <TfiClose />
+                        :
+                        <TfiMenu />
+                }
             </button>
         </div>
     );
