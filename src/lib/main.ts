693b2d4789b13566cf9cc01b6c6854bb9a1fac82
cassina <bernardocassina@gmail.com>
2023-11-16 08:46:09 -0600

    chore: add missing exports

--- src/lib/main.ts
+++ src/lib/main.ts
@@ -1,26 +1,33 @@
 // Notes
 export * from './components/notes/Button/Button';
+export * from './components/notes/CardTitle/CardTitle';
 export * from './components/notes/Checkbox/Checkbox';
-export * from './components/notes/SectionTitle/SectionTitle';
+export * from './components/notes/CheckboxShape/CheckboxShape';
 export * from './components/notes/HeroSubtitle/HeroSubtitle';
 export * from './components/notes/HeroTitle/HeroTitle';
+export * from './components/notes/Icon/Icon';
 export * from './components/notes/Label/Label';
 export * from './components/notes/Logo/Logo';
-export * from './components/chords/MobileMenuButton/MobileMenuButton';
+export * from './components/notes/SectionTitle/SectionTitle';
 export * from './components/notes/SocialIcon/SocialIcon';
-export * from './components/notes/CheckboxShape/CheckboxShape';
 
 // Chords
 export * from "./components/chords/ChoiceInput/ChoiceInput";
-export * from "@components/structures/HeroActions/HeroActions";
+export * from './components/chords/MobileMenuButton/MobileMenuButton';
 export * from "./components/chords/TextInput/TextInput";
 
 // Structures
-export * from "./components/structures/Hero/Hero";
+export * from './components/structures/Card/Card';
+export * from './components/structures/CardAction/CardAction';
+export * from './components/structures/CardBody/CardBody';
+export * from './components/structures/CardHeader/CardHeader';
+export * from './components/structures/Footer/Footer';
+export * from './components/structures/Hero/Hero';
+export * from './components/structures/HeroActions/HeroActions';
 export * from "./components/structures/Nav/Nav";
 export * from "./components/structures/NavGroup/NavGroup";
 export * from "./components/structures/NavMobileMenu/NavMobileMenu";
-
+export * from "./components/structures/SplitContainer/SplitContainer";
 
 // Config
 export * from './config/svg-paths';
